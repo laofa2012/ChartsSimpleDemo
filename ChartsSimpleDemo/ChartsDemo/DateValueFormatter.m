18aa115daf152d780d9928221033d65e03940962
faney <laofa_iphone@163.com>
2019-04-01 16:06:25 +0800

    [Feature] 更新描述 Type: 新功能 Project: ChartsSimpleDemo

--- ChartsSimpleDemo/ChartsDemo/DateValueFormatter.m
+++ ChartsSimpleDemo/ChartsDemo/DateValueFormatter.m
@@ -2,11 +2,8 @@
 //  DateValueFormatter.m
 //  ChartsDemo
 //
-//  Copyright 2015 Daniel Cohen Gindi & Philipp Jahoda
-//  A port of MPAndroidChart for iOS
-//  Licensed under Apache License 2.0
-//
-//  https://github.com/danielgindi/Charts
+//  Created by Yuanhai on 29/3/19.
+//  Copyright © 2019年 Yuanhai. All rights reserved.
 //
 
 
